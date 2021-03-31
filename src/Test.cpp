5f6f74b9e83bdb1ee118034f4e96ca8f01636129
Treidex <treidexsemextetry@gmail.com>
2021-03-31 10:12:48 -0700

    f

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,7 +1,15 @@
 #include <cstdio>
+#include <CL/cl2.hpp>
 
 __declspec(dllexport) void Test()
 {
-	std::puts("Test works!");
+	cl::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+	cl::vector<cl::Device> devices;
+	platforms.front().getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+	std::string deviceName;
+	devices.front().getInfo(CL_DEVICE_NAME, &deviceName);
+	std::printf("%s", deviceName.c_str());
 	(void)std::getchar();
 }
