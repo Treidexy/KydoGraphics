3f0e39e2ebea620610d6d44a494f504eaeea289e
Treidex <treidexsemextetry@gmail.com>
2021-04-01 14:36:16 -0700

    help

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -24,25 +24,30 @@ int FindCommonDenominator(int x, int y)
 void DrawLine(COLORREF *pixels, const Kydo::Vertex &v0, const Kydo::Vertex &v1)
 {
 	const Kydo::Vertex &min = (v0.Y < v1.Y) ? v0 : v1;
-	const Kydo::Vertex &max = (v0.Y > v1.Y) ? v0 : v1;
+	const Kydo::Vertex &max = (v0.Y < v1.Y) ? v1 : v0;
 
-	UINT dx = max.X - min.X;
-	UINT dy = max.Y - min.Y;
+	INT dx = min.X < max.X ? max.X - min.X : min.X - max.X;
+	INT dy = max.Y - min.Y;
 
+	if (dx == 0)
+		dx = 1;
 	if (dy == 0)
 		dy = 1;
 
-	INT tror = (dx * min.Y) / dy;
-	INT b = min.X - tror;
+	INT ror = (dx * min.Y) / dy;
+	INT b = min.X - ror;
 
 	for (UINT y = min.Y; y <= max.Y; y++)
 	{
-		UINT ror = (dx * y) / dy;
-		
-		UINT minX = ror + b;
-		UINT maxX = ror + b;
-		for (UINT x = minX; x <= maxX; x++)
-			pixels[x + (512 - y - titleBarHeight - 1) * 512] = 0x00FF00;
+		INT ror = (dx * y) / dy;
+		INT minX = ror + b;
+		INT iror = (dx * (minX)) / dy;
+		INT maxX = iror + b;
+
+
+		std::printf("%i, %i\n", minX, maxX);
+		//for (UINT x = minX; x <= maxX; x++)
+		//	pixels[x + (512 - y - titleBarHeight - 1) * 512] = 0x00FF00;
 	}
 }
 
@@ -65,9 +70,9 @@ int main()
 		//if (renderer->IsAlive())
 		//{
 			wnd.Show();
-			DrawLine(wnd.Pixels, { 256, 128 }, { 128, 384 });
+			//DrawLine(wnd.Pixels, { 256, 128 }, { 128, 384 });
 			DrawLine(wnd.Pixels, { 128, 384 }, { 384, 384 });
-			DrawLine(wnd.Pixels, { 384, 384 }, { 256, 128 });
+			//DrawLine(wnd.Pixels, { 384, 384 }, { 256, 128 });
 			//renderer->Render(
 			//	{
 			//		256, 128, 384,
